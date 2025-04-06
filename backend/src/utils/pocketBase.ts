1a8592f1ba1822b2050d972e46c9b2b98f1e51d1
ilzbergs <ilzbergs@inbox.lv>
2025-04-06 19:02:56 +0300

    some code updates

--- backend/src/utils/pocketBase.ts
+++ backend/src/utils/pocketBase.ts
@@ -1,6 +1,10 @@
 import PocketBase from 'pocketbase';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase(
+  process.env.POCKETBASE_URL || 'http://127.0.0.1:8090'
+);
 pb.autoCancellation(false);
 
 export default pb;
