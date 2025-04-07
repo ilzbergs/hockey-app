59b8bdbbbff17353a96618bbc8dd42c165174145
ilzbergs <ilzbergs@inbox.lv>
2025-04-07 17:38:13 +0300

    added dockerfile

--- backend/src/utils/pocketBase.ts
+++ backend/src/utils/pocketBase.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 const pb = new PocketBase(
-  process.env.POCKETBASE_URL || 'http://127.0.0.1:8090'
+  process.env.POCKETBASE_URL || 'http://127.0.0.1:8080'
 );
 pb.autoCancellation(false);
 
