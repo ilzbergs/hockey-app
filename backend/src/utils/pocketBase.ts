a7450fa754a92b1066d96cba6a6df73524cdf27c
ilzbergs <ilzbergs@inbox.lv>
2025-04-08 21:46:35 +0300

    added pocketbase admin token

--- backend/src/utils/pocketBase.ts
+++ backend/src/utils/pocketBase.ts
@@ -1,8 +1,11 @@
 import PocketBase from 'pocketbase';
 
+const adminToken = 'tavs-admin-token';
+
 const pb = new PocketBase(
   process.env.POCKETBASE_URL || 'http://127.0.0.1:8080'
 );
+pb.authStore.save(adminToken);
 pb.autoCancellation(false);
 
 export default pb;
