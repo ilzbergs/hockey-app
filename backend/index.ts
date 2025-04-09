c7b69473078cb1a20c2ead2425d1e6f5ecc79e85
ilzbergs <ilzbergs@inbox.lv>
2025-04-09 16:06:27 +0300

    hope it works!

--- backend/index.ts
+++ backend/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 // CORS options to allow requests from the frontend
 const corsOptions = {
-  origin: '/https://hockey-app.vercel.app/',
+  origin: 'https://hockey-app.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
