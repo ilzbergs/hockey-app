2afbf156fc97e1e438c2fb63b45c7d6ebdc6eea7
ilzbergs <ilzbergs@inbox.lv>
2025-04-29 17:22:36 +0300

    added cors options

--- backend/index.ts
+++ backend/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 // CORS options to allow requests from the frontend
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'http://65.109.11.244:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
