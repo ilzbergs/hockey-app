8845096f87979fce8cf236b1ac5317a0bda33f19
ilzbergs <ilzbergs@inbox.lv>
2025-04-29 19:01:05 +0300

    some changes

--- backend/webpack.config.mjs
+++ backend/webpack.config.mjs
@@ -1,7 +1,5 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import dotenv from 'dotenv';
-// dotenv.config();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
