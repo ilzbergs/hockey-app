7fcf41ee44cc14b41dca1d7efc33b95b5b3f9676
ilzbergs <ilzbergs@inbox.lv>
2026-02-06 15:52:42 +0200

    update time again

--- frontend/src/views/UserPredictions.vue
+++ frontend/src/views/UserPredictions.vue
@@ -33,7 +33,7 @@
       v-if="!authStore.user.predictionActive"
       v-model="games"
       mode="add"
-      :addEndDate="new Date('2026-01-11T17:40:00')"
+      :addEndDate="new Date('2026-02-11T17:40:00')"
     />
     <Predictions v-else v-model="predictions" mode="list" />
   </div>
