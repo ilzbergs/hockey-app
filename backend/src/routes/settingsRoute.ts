e3d020dd34df671f971e284151ea19e53609dcdd
ilzbergs <ilzbergs@inbox.lv>
2026-04-21 22:40:35 +0300

    some again

--- backend/src/routes/settingsRoute.ts
+++ backend/src/routes/settingsRoute.ts
@@ -4,6 +4,6 @@ import { checkAuth } from '../middleware/authenticate';
 
 const router = Router();
 
-router.get('/',checkAuth, getSettings);
+router.get('/settings',checkAuth, getSettings);
 
 export default router;
